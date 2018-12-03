ee7bee4ea9d94e122f4bacc80d4685125dbe4e46
Valentyn Vasylenko <vasylenko.valentyn@gmail.com>
2018-12-03 18:22:32 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyDvDawJd8aoqFPufOK2auL4Z8wU95JfL20',
+  authDomain: 'halpq-project.firebaseapp.com',
+  databaseURL: 'https://halpq-project.firebaseio.com',
+  projectId: 'halpq-project',
+  storageBucket: 'halpq-project.appspot.com',
+  messagingSenderId: '408437676969',
+};
+firebase.initializeApp(config);
+
+export default firebase;
