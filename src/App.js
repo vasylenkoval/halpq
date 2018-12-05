2edbe7e1e7d8737727b197185ade02fa5144db96
Valentyn Vasylenko <vasylenko.valentyn@gmail.com>
2018-12-05 14:38:34 -0500

    Method to convert admin to student

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import firebase from './firebase';
 
 const provider = new firebase.auth.GoogleAuthProvider();
 const auth = firebase.auth();
+const dbRef = firebase.database();
 
 class App extends Component {
   constructor() {
@@ -25,6 +26,7 @@ class App extends Component {
       }
       // Check if user is an Admin
       this.isAdmin();
+      this.makeStudent('800RdWEbEGPUxV3ef8y24CoYIa02');
     });
   }
 
@@ -62,8 +64,7 @@ class App extends Component {
 
   // Method to check if the user is an Admin
   isAdmin = () => {
-    firebase
-      .database()
+    dbRef
       .ref(`/Users/Admins/${this.state.user.uid}`)
       .once('value', snapshot => {
         if (snapshot.exists()) {
@@ -77,97 +78,93 @@ class App extends Component {
       });
   };
 
-  // Convert user to an admin
+  // Convert student to an admin
   makeAdmin = studentUID => {
-    firebase
-      .database()
-      .ref(`/Users/Students/${studentUID}`)
-      .once('value', snapshot => {
-        if (snapshot.exists()) {
-          firebase
-            .database()
-            .ref(`/Users/Admins/${studentUID}`)
-            .set(snapshot.val());
-          firebase
-            .database()
-            .ref(`/Users/Students/${studentUID}`)
-            .remove();
-          console.log(
-            'USER IS PRESENT IN STUDENTS REF, MOVING TO ADMIN AND DELETING FROM STUDENTS'
-          );
-        } else {
-          console.log('NO SUCH USER');
-        }
-      });
+    dbRef.ref(`/Users/Students/${studentUID}`).once('value', snapshot => {
+      if (snapshot.exists()) {
+        dbRef.ref(`/Users/Admins/${studentUID}`).set(snapshot.val());
+        dbRef.ref(`/Users/Students/${studentUID}`).remove();
+        console.log(
+          'USER IS PRESENT IN STUDENTS REF, MOVING TO ADMIN AND DELETING FROM STUDENTS'
+        );
+      } else {
+        console.log('NO SUCH USER');
+      }
+    });
+  };
+
+  // Conver Admin to Student
+
+  makeStudent = adminUID => {
+    dbRef.ref(`/Users/Admins/${adminUID}`).once('value', snapshot => {
+      if (snapshot.exists()) {
+        dbRef.ref(`/Users/Students/${adminUID}`).set(snapshot.val());
+        dbRef.ref(`/Users/Admins/${adminUID}`).remove();
+        console.log(
+          'USER IS PRESENT IN STUDENTS REF, MOVING TO ADMIN AND DELETING FROM STUDENTS'
+        );
+      } else {
+        console.log('NO SUCH ADMIN');
+      }
+    });
   };
 
   // Testing
 
   // Method to create a classroom - only avilable to admins in Classroom List view.
   createTestClassRoom = () => {
-    firebase
-      .database()
-      .ref(`/Classrooms/`)
-      .push({
-        classRoomName: 'Test Classroom',
-        classRoomQuestions: 0,
-        enrolledStudents: 0,
-      });
+    dbRef.ref(`/Classrooms/`).push({
+      classRoomName: 'Test Classroom',
+      classRoomQuestions: 0,
+      enrolledStudents: 0,
+    });
   };
 
   // Method to create a question in the classroom - needs a classroom reference. Should be moved to the HelpCue view.
   createTestQuestion = classroomRef => {
-    firebase
-      .database()
-      .ref(`/Classrooms/${classroomRef}/classRoomQuestions`)
-      .push({
-        name: this.state.user.displayName,
-        content: 'halp!',
-        uid: this.state.user.uid,
-        photoURL: this.state.user.photoURL,
-        dateCreated: +new Date(),
-        dateHelped: 0,
-        dateCompleted: 0,
-        isCompleted: false,
-        location: 0,
-      });
+    dbRef.ref(`/Classrooms/${classroomRef}/classRoomQuestions`).push({
+      name: this.state.user.displayName,
+      content: 'halp!',
+      uid: this.state.user.uid,
+      photoURL: this.state.user.photoURL,
+      dateCreated: +new Date(),
+      dateHelped: 0,
+      dateCompleted: 0,
+      isCompleted: false,
+      location: 0,
+    });
   };
 
   // Method to enroll in the classroom - should be moved to the Classroom List view.
   // Consider creating a duplicate shallow reference in the DB to store just the keys of the classrooms to prevent from downloading all nested data
   testEnroll = enrollPassword => {
-    firebase
-      .database()
-      .ref(`/Classrooms`)
-      .once('value', snapshot => {
-        const classroomMatch = Object.entries(snapshot.val()).filter(element =>
-          element[0].includes(enrollPassword)
-        );
-        console.log(classroomMatch);
-        if (classroomMatch.length > 0) {
-          // If there is a match - record a student in classroom ref
-          firebase
-            .database()
-            .ref(
-              `/Classrooms/${classroomMatch[0][0]}/enrolledStudents/${
-                this.state.user.uid
-              }`
-            )
-            .set('TEST');
+    dbRef.ref(`/Classrooms`).once('value', snapshot => {
+      const classroomMatch = Object.entries(snapshot.val()).filter(element =>
+        element[0].includes(enrollPassword)
+      );
+      console.log(classroomMatch);
+      if (classroomMatch.length > 0) {
+        // If there is a match - record a student in classroom ref
+        dbRef
+          .ref(
+            `/Classrooms/${classroomMatch[0][0]}/enrolledStudents/${
+              this.state.user.uid
+            }`
+          )
+          .set('TEST');
 
-          // Also record a class key in student's own profile
-          firebase
-            .database()
-            .ref(
-              `/Users/Students/${this.state.user.uid}/enrolledClasses/${
-                classroomMatch[0][0]
-              }`
-            )
-            .set('TEST');
-        } else {
-          console.log('Wrong key!');
-        }
-      });
+        // Also record a class key in student's own profile
+        dbRef
+          .ref(
+            `/Users/Students/${this.state.user.uid}/enrolledClasses/${
+              classroomMatch[0][0]
+            }`
+          )
+          .set('TEST');
+      } else {
+        console.log('Wrong key!');
+      }
+    });
   };
 
   render() {
