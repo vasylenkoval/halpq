b9b2142148bcbe8bb5b001460ec216b1b04e7ca3
Amy <amy_ludwig@live.com>
2018-12-05 18:30:13 -0500

    this is a commit

---
+++ src/Components/AdminList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AdminList extends Component {
+  render() {
+    return (
+      // map list of admins on firebase:
+      <div className="admin">
+        <p>Admins</p>
+      </div>
+    );
+  }
+}
+
+export default AdminList;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Header extends Component {
   constructor() {
-    console.log("the constructor in Header was called");
+    console.log('the constructor in Header was called');
     super();
     this.state = {};
   }
+
+  // Render will include sub components of H1, userManagementLink, user picture and logout button.
   render() {
-      return(
-        
-      )
+    return (
+      <div>
+        <h1>henlo</h1>
+      </div>
+    );
   }
 }
 
