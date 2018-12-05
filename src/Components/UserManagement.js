2dbc32df96be8472f8c31c462ac98ca528b51b10
DanDitomaso <dan.ditomaso@gmail.com>
2018-12-05 15:25:26 -0500

    Build framework for all top level components

---
+++ src/Components/UserManagement.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class UserManagement extends Component {
+  constructor() {
+    console.log("the constructor in User Management was called");
+    super();
+    this.state = {};
+  }
+  render() {
+      return(
+        
+      )
+  }
+}
+
+export default UserManagement;
