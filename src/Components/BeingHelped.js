c27acc7b21fa71b7f800bb2e55519706e5db99a1
Amy <amy_ludwig@live.com>
2018-12-09 18:42:14 -0500

    added being helped. will change firebase to saying being helped =true

--- src/Components/BeingHelped.js
+++ src/Components/BeingHelped.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
-// import firebase from "firebase";
+import React, { Component } from "react";
+import firebase from "firebase";
 
 class BeingHelped extends Component {
+  beingHelped = (classroomRef, questionRef) => {
+    const question = firebase
+      .database()
+      .ref(`/Questions/${classroomRef}/${questionRef}/isBeingHelped`);
+    question.set(true);
+  };
+
   render() {
     return (
       <div>
-        <button type="button">BEING HELPED</button>
+        <button
+          type="button"
+          onClick={() => {
+            this.beingHelped(this.props.classKey, this.props.questionKey);
+          }}
+        >
+          BEING HELPED
+        </button>
       </div>
     );
   }
