2c57d59df6ef9962163c6de2fbcd25d661208f89
Amy <amy_ludwig@live.com>
2018-12-10 12:26:54 -0500

    added some styling

--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { makeAdmin } from './globalFunctions';
+import arrow from '../assets/arrow-right.svg';
 
 class StudentList extends Component {
   render() {
     return (
       // map list of students on firebase:
 
-      <div className="student">
+      <div className="userList">
         {this.props.students.map(student => (
-          <div key={student[0]} className="studentInfo">
-            <h3>{student[1].displayName}</h3>
-            <p>{student[1].email}</p>
-            <button
-              type="button"
-              onClick={() => {
-                makeAdmin(`${student[0]}`);
-              }}
-            >
-              Make Baller
-            </button>
+          <div key={student[0]} className="userList__user student clearfix">
+            <div className="user__details">
+              <h3>{student[1].displayName}</h3>
+              <p>{student[1].email}</p>
+            </div>
+            <div className="user__action">
+              <button className="user__button"
+                type="button"
+                onClick={() => {
+                  makeAdmin(`${student[0]}`);
+                }}
+              >
+                Baller-ize
+                <div className="buttonImage">
+                  <img src={arrow} alt="make user a student" />
+                </div>
+              </button>
+            </div>
           </div>
         ))}
       </div>
