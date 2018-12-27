be2a852fff2dc379c985f43d499fc0c988abbbb7
DanDitomaso <dan.ditomaso@gmail.com>
2018-12-26 19:03:01 -0500

    Added user search functionality to user management page

--- src/Components/UserManagement.js
+++ src/Components/UserManagement.js
@@ -14,13 +14,13 @@ class UserManagement extends Component {
       adminList: [],
       studentCount: 0,
       adminCount: 0,
-      userInput: ""
+      search: ""
     };
   }
 
   handleChange = (e) => {
     this.setState({
-      userInput: e.target.value
+      search: e.target.value.substr(0, 20)
     });
   };
 
@@ -32,7 +32,7 @@ class UserManagement extends Component {
         this.setState({ studentList: [] });
       } else if (snapshot.val()) {
         const newstudentList = snapshot.val();
-        const studentList = Object.entries(newstudentList).sort();
+        const studentList = Object.entries(newstudentList);
         const studentCount = Object.entries(newstudentList).length;
         this.setState({ studentList, studentCount });
       }
@@ -42,7 +42,7 @@ class UserManagement extends Component {
         this.setState({ adminList: [] });
       } else if (snapshot.val()) {
         const myadminList = snapshot.val();
-        const adminList = Object.entries(myadminList).sort();
+        const adminList = Object.entries(myadminList);
         const adminCount = Object.entries(myadminList).length;
         this.setState({ adminList, adminCount });
       }
@@ -57,14 +57,25 @@ class UserManagement extends Component {
   }
 
   render() {
+    let filteredAdminUserList = this.state.adminList.filter((user) => {
+      return (
+        user[1].displayName
+          .toLowerCase()
+          .indexOf(this.state.search.toLowerCase()) !== -1
+      );
+    });
+    let filteredStudentUserList = this.state.studentList.filter((user) => {
+      return (
+        user[1].displayName
+          .toLowerCase()
+          .indexOf(this.state.search.toLowerCase()) !== -1
+      );
+    });
     return (
       <div className="userManagement">
         <div className="wrapper">
           <div className="Component__Title">
             <h2>User Management</h2>
-          </div>
-          <div className="users__search">
-            <input type="search" name="" id="" onChange={this.handleChange} />
           </div>
           <Link className="backLink" to="/">
             <div className="returnLink clearfix">
@@ -74,14 +85,25 @@ class UserManagement extends Component {
               <p>Back to Classlist</p>
             </div>
           </Link>
+          <div className="users__search">
+            <label htmlFor="userSearch">User Search: </label>
+            <input
+              type="search"
+              name="userSearch"
+              id="userSearch"
+              placeholder="Search for a user"
+              results={5}
+              onChange={this.handleChange}
+            />
+          </div>
           <div className="users clearfix">
             <div className="users__panel">
               <h3 className="users__panelTitle users__panelTitle--admin">
                 Admins
               </h3>
               <span className="users__count">({this.state.adminCount})</span>
-              {this.state.adminList[0] ? (
-                <AdminList admins={this.state.adminList} />
+              {this.state.adminList[0] ? ( // <AdminList admins={this.state.adminList} />
+                <AdminList admins={filteredAdminUserList} />
               ) : (
                 <p className="noAdmin">There are no admins</p>
               )}
@@ -90,7 +112,7 @@ class UserManagement extends Component {
               <h3 className="users__panelTitle">Students</h3>
               <span className="users__count">({this.state.studentCount})</span>
               {this.state.studentList[0] ? (
-                <StudentList students={this.state.studentList} />
+                <StudentList students={filteredStudentUserList} />
               ) : (
                 <p>There are no students</p>
               )}
